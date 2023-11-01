43730b20386a0c595b96783c71d64094bb24a0b0
Alstudd <alstonsoares17@gmail.com>
2023-11-01 20:52:22 +0530

    static files rendering on server try-1

--- detect.py
+++ detect.py
@@ -29,7 +29,7 @@ from utils.torch_utils import select_device, load_classifier, time_sync
 @torch.no_grad()
 def run(weights='yolov5s.pt',  # model.pt path(s)
         source='data/images',  # file/dir/URL/glob, 0 for webcam
-        imgsz=640,  # inference size (pixels)
+        imgsz=608,  # inference size (pixels)
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
